61bf80c7364ede5fada240520b3d2a44ecb7064b
PlenipotentSS <stevensonhoyt@icloud.com>
2014-01-30 10:49:28 -0800

    moved dependencies for testing

---
+++ Roster AppTests/TestBot.m
@@ -0,0 +1,34 @@
+//
+//  TestBot.m
+//  Roster App
+//
+//  Created by Stevenson on 1/30/14.
+//  Copyright (c) 2014 Steven Stevenson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestBot : XCTestCase
+
+@end
+
+@implementation TestBot
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
